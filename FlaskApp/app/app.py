357699fc7366c325ac59e7cbf27a7f00965cbd2f
jfponce1 <elisejohn1995@gmail.com>
2023-01-24 22:46:49 -0500

    Added Celery
    Added celery Framework

--- FlaskApp/app/app.py
+++ FlaskApp/app/app.py
@@ -1,12 +1,17 @@
 from flask import Flask,render_template,request,redirect,url_for,flash
 from readingAlgorithm.readingAlgorith import predict
 from flowRecorder import watchInterface,run_by_web
-
+from celery_config import make_celery
+from celery import result
+#import jsonpickle
 
 app= Flask(__name__)
+app.config['CELERY_BROKER_URL']='amqp://localhost//'
+app.config['CELERY_RESULT_BACKEND']='db+mysql://root:@localhost:3306/test2'
 
 app.secret_key='mysecretkey'
 
+celery=make_celery(app)
 
 @app.route('/')
 def index():
@@ -38,7 +43,10 @@ def background_process_test():
     data={'tipo': 3
     }
     flash(data)
-    run_by_web(4)
+    result=listen_flow.delay(4)
+    #result=reverse.delay('john')
+    #result.wait()
+    print(result.state)
     return ("nothing")
 
 @app.route('/reviewInterfaces',methods=['POST'])
@@ -63,6 +71,19 @@ def query_string():
 def pagina_no_encontrada(error):
     #return render_template("404.html"),404
     return redirect(url_for('index'))
+
+@celery.task(name='celery_example.listen_flow')
+def listen_flow(num):
+    #return string
+    #update_state(state='PROGRESS')
+    run_by_web(num)
+    return 'Do it '
+
+@celery.task(name='celery_example.reverse')
+def reverse(string):
+    #return string
+    print(string)
+    return string[::-1]
 
 if __name__=='__main__':
     app.add_url_rule('/query_string',view_func=query_string)

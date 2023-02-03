73bef5be1c699bf711456a2c7849124a9a71b1a1
Luigi Villarreal Campaña <ldvillarreal1@espe.edu.ec>
2023-02-03 09:18:56 -0500

    Cambios en la interfaz
    Cambios realizados en la interfaz

--- FlaskApp/app/app.py
+++ FlaskApp/app/app.py
@@ -3,11 +3,13 @@ from readingAlgorithm.readingAlgorith import predict
 from flowRecorder import watchInterface,run_by_web
 from celery_config import make_celery
 from celery import result
+
+from flask import request
 #import jsonpickle
 
 app= Flask(__name__)
 app.config['CELERY_BROKER_URL']='amqp://localhost//'
-app.config['CELERY_RESULT_BACKEND']='db+mysql://root:@localhost:3306/test2'
+app.config['CELERY_RESULT_BACKEND']='db+mysql://root:root@localhost:3306/analizer'
 
 app.secret_key='mysecretkey'
 
@@ -36,40 +38,127 @@ def contacto(nombre,edad):
     }
     return render_template('contacto.html',data=data)
 
-#background process happening without any refreshing
-@app.route('/background_process_test')
-def background_process_test():
-    #predict()
-    data={'tipo': 3
-    }
-    flash(data)
-    result=listen_flow.delay(4)
-    #result=reverse.delay('john')
-    #result.wait()
-    print(result.state)
-    return ("nothing")
+@app.route('/shutdown')
+def shutdown():
 
-@app.route('/reviewInterfaces',methods=['POST'])
-def method_name():
-    print ("Hello .........")
+    # If not logged in, return back to the login page.
+    if not session or not session['logged_in']:
+        return render_template('index.html')
+
+    # Logged_in, continue...
+    sys.exit()
+    os.exit(0)
+    return
+
+# MENU OPCIONES
+@app.route('/menu/<menuNav>/<analisisMenu>')
+def menu_inicio(menuNav,analisisMenu):
+    
+    menuOp = int(format(menuNav))
+    analisisOp = int(format(analisisMenu))
+    print ("Inicio .........")
     #predict()
     interfaces=watchInterface()
+
+    puertos = list()
+    for i in range(len(interfaces)):
+        puertos.append(
+            {
+                "id": i, 
+                "dato": interfaces[i]
+            }
+        )
+
+    print('puertos: ',puertos)
+
     print('len: ',len(interfaces))
-    data={'tipo': 1,
-    'dato': interfaces
+
+    data={
+        'menu': menuOp,
+        'analisisMenu': analisisOp,
+        'tipo': 1,
+        'interfaces': puertos
     }
+
+    flash(data)
+    print("bye")
+    return redirect(url_for('index'))
+    
+
+@app.route('/reviewInterfaces/<menuNav>/<analisisMenu>',methods=['POST'])
+def method_name(menuNav,analisisMenu):
+    print ("Hello .........")
+    #predict()
+
+    menuOp = int(format(menuNav))
+    analisisOp = int(format(analisisMenu))
+    print ("Inicio .........")
+    #predict()
+    interfaces=watchInterface()
+
+    puertos = list()
+    for i in range(len(interfaces)):
+        puertos.append(
+            {
+                "id": i, 
+                "dato": interfaces[i]
+            }
+        )
+
+    print('puertos: ',puertos)
+
+    print('len: ',len(interfaces))
+
+    data={
+        'menu': menuOp,
+        'analisisMenu': analisisOp,
+        'tipo': 1,
+        'interfaces': puertos
+    }
+
     flash(data)
     print("bye")
     return redirect(url_for('index'))
 
-def query_string():
-    print(request)
-    print(request.args)
-    print(request.args.get('param1'))
-    return "Ok"
+#background process happening without any refreshing
+@app.route('/background_process_test/<param>/<menuNav>/<analisisMenu>')
+def background_process_test(param,menuNav,analisisMenu):
+    #predict()
+    #param = request.arg.get('params1','SIN PARAMETROS')
 
-def pagina_no_encontrada(error):
-    #return render_template("404.html"),404
+    puerto = int(format(param))
+    menuOp = int(format(menuNav))
+    analisisOp = int(format(analisisMenu))
+
+    interfaces=watchInterface()
+
+    puertos = list()
+    for i in range(len(interfaces)):
+        puertos.append(
+            {
+                "id": i, 
+                "dato": interfaces[i]
+            }
+        )
+
+    print('puertos: ',puertos)
+
+    print('len: ',len(interfaces))
+
+    data={
+        'menu': menuOp,
+        'analisisMenu': analisisOp,
+        'tipo': 1,
+        'interfaces': puertos
+    }
+    
+    flash(data)
+    result=listen_flow.delay((int(puerto)))
+    #result=reverse.delay('john')
+    #result.wait()
+    print(result.state)
+    print(result)
+    # return ("nothing")
     return redirect(url_for('index'))
 
 @celery.task(name='celery_example.listen_flow')
@@ -84,6 +173,18 @@ def reverse(string):
     #return string
     print(string)
     return string[::-1]
+
+
+
+def query_string():
+    print(request)
+    print(request.args)
+    print(request.args.get('param1'))
+    return "Ok"
+
+def pagina_no_encontrada(error):
+    #return render_template("404.html"),404
+    return redirect(url_for('index'))
 
 if __name__=='__main__':
     app.add_url_rule('/query_string',view_func=query_string)
